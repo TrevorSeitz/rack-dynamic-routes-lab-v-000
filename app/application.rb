b06a92e7d2c32a7e56e1c0a2fe21fd4b9ea84bf4
Trevor Seitz <trevinla@gmail.com>
2018-04-25 15:16:54 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
 
     if req.path=="/item/<Item Name>"
       resp.write "The item costs"
-    elsif @@item.include?
+    elsif @@items.include?
       resp.write "Item not found"
       resp status = 400
     else
